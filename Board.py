a20f1d60d092079c7960c71db41f704c3f6dd02d
nguyentuthanhdn <nguyentuthanhdn@gmail.com>
2021-06-01 21:53:20 -0600

    Add simple wall

--- Board.py
+++ Board.py
@@ -3,13 +3,17 @@ from typing import Tuple
 
 class Board:
     def __init__(self) -> None:
-        self.board = [[0] * 20 for i in range(20)]
-        self.destination_x = 19
-        self.destination_y = 19
+        self.SIZE = 5
+        self.board = [[0] * self.SIZE for i in range(self.SIZE)]
+        self.destination_x = self.SIZE - 1
+        self.destination_y = self.SIZE - 1
         self.x = 0
         self.y = 0
         self.board[self.x][self.y] = "X"
         self.board[self.destination_x][self.destination_y] = "D"
+        
+        for i in range(self.SIZE - 1):
+            self.board[i][2] = 1
     
     def getCoordinate(self) -> Tuple[int, int]:
         return self.x, self.y
